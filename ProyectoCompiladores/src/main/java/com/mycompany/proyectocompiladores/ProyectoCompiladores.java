124c584a1b262f277b463a49f49a069942dc9d3b
Guilmar24 <91397336+Guilmar24@users.noreply.github.com>
2025-04-04 23:03:31 -0600

    se agrega el proyecto .java

---
+++ ProyectoCompiladores/src/main/java/com/mycompany/proyectocompiladores/ProyectoCompiladores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocompiladores;
+
+/**
+ *
+ * @author kinch
+ */
+public class ProyectoCompiladores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
